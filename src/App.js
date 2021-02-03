1a09d691726b0d89b1353b6c761aad3317b1903b
Valmil <valmil.silva@sulamerica.com.br>
2021-02-03 00:06:43 -0300

    terceiro commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AWS Amplify <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
